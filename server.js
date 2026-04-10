d3be61f70385a077ffdaa86552094ba00f9a2b6b
Techbro <bolaolat001@gmail.com>
2026-04-10 20:28:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,12 @@ const http = require("http");
 const WebSocket = require("ws");
 
 const PORT = process.env.PORT || 3000;
-
-// Create HTTP server (REQUIRED for Render)
 const server = http.createServer();
-
-// Attach WebSocket to HTTP server
 const wss = new WebSocket.Server({ server });
 
 let players = [];
 
 wss.on("connection", (ws) => {
-  console.log("Player connected");
   players.push(ws);
 
   ws.on("message", (msg) => {
@@ -24,12 +19,10 @@ wss.on("connection", (ws) => {
   });
 
   ws.on("close", () => {
-    console.log("Player disconnected");
     players = players.filter(p => p !== ws);
   });
 });
 
-// Start server properly
 server.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
